1fc5814fb43632ed559b301a44c04cdcde52303c
Carl-Magnus <carl.magnus@hotmail.com>
2021-01-14 03:34:35 +0100

    Player movement, weapon switching and gitignore

---
+++ Recoil Away/Assets/Scripts/PlayerScripts/MovementScripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement Values")]
+    public float speed;
+    public float jumpForce;
+
+    private float moveInput;
+
+    Vector2 velocity;
+
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();   
+    }
+
+    private void Move()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+
+        velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        rb.velocity = velocity;
+    }
+}
