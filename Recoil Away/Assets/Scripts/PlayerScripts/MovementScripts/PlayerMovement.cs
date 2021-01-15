df481434a9bf27555215240940db3393f7986140
Carl-Magnus <carl.magnus@hotmail.com>
2021-01-15 02:41:28 +0100

    Added jumping mechanics as well as the new model for the player, and the latest spritesheet.

--- Recoil Away/Assets/Scripts/PlayerScripts/MovementScripts/PlayerMovement.cs
+++ Recoil Away/Assets/Scripts/PlayerScripts/MovementScripts/PlayerMovement.cs
@@ -7,29 +7,86 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement Values")]
     public float speed;
     public float jumpForce;
+    public int extraJumps;
+    private int jumpReset;
 
     private float moveInput;
+    private float checkRadius = 0.5f;
+
+    private bool facingRight = true;
+    private bool isGrounded;
+
 
     Vector2 velocity;
 
     Rigidbody2D rb;
 
+    public Transform groundCheck;
+
+    public LayerMask whatIsGround;
+
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+
+        jumpReset = extraJumps;
     }
 
     void Update()
     {
-        Move();   
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
+        {
+            Jump();
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded)
+        {
+            Jump();
+        }
+
+        if (isGrounded)
+        {
+            extraJumps = jumpReset;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
     }
 
     private void Move()
     {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
         moveInput = Input.GetAxisRaw("Horizontal");
 
         velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
         rb.velocity = velocity;
+
+        if (!facingRight && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+    private void Jump()
+    {
+        velocity = Vector2.up * jumpForce;
+        rb.velocity = velocity;
+        extraJumps--;
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scalar = transform.localScale;
+        scalar.x *= -1;
+        transform.localScale = scalar;
     }
 }
